eb73c3c20827481d409980169bbf9138d52bf363
Frederik Huss Nielsen <au600953@post.au.dk>
2023-12-11 18:58:06 +0100

    AOC day 9 solution

--- ninth/src/main.rs
+++ ninth/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::iter;
 use std::path::Path;
 use itertools::Itertools;
 
@@ -17,7 +18,39 @@ fn solution() {
         }
 
         //println!("{:?}", histories);
+        let mut sum: i32 = 0;
+        for history in histories {
+            let differences = differences(history.clone());
+            //println!("{:?} {:?}", histories[0], differences);
+            let prediction = predict_history_backwards(history.clone(), differences);
+            //println!("{}", prediction);
+            sum += prediction;
+        }
+
+        println!("{}", sum);
     }
+}
+
+fn differences(history: Vec<i32>) -> Vec<Vec<i32>> {
+    iter::repeat(69).scan(history, |state, _| {
+        if state.iter().all(|value| value == &0) {
+            return None;
+        }
+
+        state.iter().tuple_windows::<(_,_)>().map(|window| window.1 - window.0).collect_vec().clone_into(state);
+        //println!("{:?}", state);
+
+        Some(state.clone())
+    }).collect_vec()
+}
+
+#[allow(dead_code)]
+fn predict_history_forwards(first: Vec<i32>, next: Vec<Vec<i32>>) -> i32 {
+    next.iter().rev().fold(0, |prediction, values| values.last().unwrap() + prediction) + first.last().unwrap()
+}
+
+fn predict_history_backwards(first: Vec<i32>, next: Vec<Vec<i32>>) -> i32 {
+    first.first().unwrap() - next.iter().rev().fold(0, |prediction, values| values.first().unwrap() - prediction)
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
